40dc48b53476767bced1ae6c66cb3bf0edde8d4c
AnastasiaPetasova <petasovan@gmail.com>
2017-12-05 12:06:19 +0400

    Добавлены мета-комманды выполнения: - .clear - очистить все переменные - .print - вывести значения всех переменных - .end - закончить выполнение

--- src/com/anastasia/app/multiarray/Main.java
+++ src/com/anastasia/app/multiarray/Main.java
@@ -1,8 +1,40 @@
 package com.anastasia.app.multiarray;
 
+import com.anastasia.app.multiarray.eval.MultiArrayEnvironment;
+
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static final String META_COMMAND_PREFIX = ".";
+    public static final String CLEAR = "clear", END = "end", PRINT = "print";
 
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        MultiArrayEnvironment environment = MultiArrayEnvironment.create();
+
+        evaluation:
+        while (true) {
+            String line = in.nextLine();
+            if (line == null) break;
+
+            if (line.startsWith(META_COMMAND_PREFIX)) {
+                String command = line.substring(META_COMMAND_PREFIX.length());
+                switch (command) {
+                    case CLEAR:
+                        environment.clear();
+                        break;
+                    case END:
+                        break evaluation;
+                    case PRINT:
+                        String environmentString = environment.toString();
+                        System.out.println(environmentString);
+                        break;
+                }
+            } else {
+
+            }
+        }
     }
 }
