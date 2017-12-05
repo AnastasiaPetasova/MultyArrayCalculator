1d5a1e0331c3e28aa150d4c01c696b19622df05c
AnastasiaPetasova <petasovan@gmail.com>
2017-12-05 11:49:15 +0400

    В MultiArrayEnvironment добавлены функции очистки переменных, получения и установки значения переменной

--- src/com/anastasia/app/multiarray/eval/MultiArrayEnvironment.java
+++ src/com/anastasia/app/multiarray/eval/MultiArrayEnvironment.java
@@ -1,5 +1,36 @@
 package com.anastasia.app.multiarray.eval;
 
+import com.anastasia.app.multiarray.array.MultiArray;
+import com.anastasia.app.multiarray.validation.EvaluationException;
+import com.anastasia.app.multiarray.validation.EvaluationValidator;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class MultiArrayEnvironment {
 
+    public static MultiArrayEnvironment create() {
+        return new MultiArrayEnvironment();
+    }
+
+    Map<String, MultiArray> variables;
+
+    private MultiArrayEnvironment() {
+        this.variables = new HashMap<>();
+    }
+
+    public void clear() {
+        this.variables.clear();
+    }
+
+    public MultiArray get(String name) throws EvaluationException {
+        MultiArray value = variables.get(name);
+        EvaluationValidator.assertVariableExists(value, name);
+
+        return value;
+    }
+
+    public void set(String name, MultiArray value) {
+        variables.put(name, value);
+    }
 }
