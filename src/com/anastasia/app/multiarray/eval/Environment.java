d2a39272a0730722b94fb5a1b58d6a0c7ffc7131
AnastasiaPetasova <petasovan@gmail.com>
2017-12-05 13:09:58 +0400

    Константы операций перенесены в Environment

--- src/com/anastasia/app/multiarray/eval/Environment.java
+++ src/com/anastasia/app/multiarray/eval/Environment.java
@@ -4,6 +4,10 @@ import com.anastasia.app.multiarray.validation.EvaluationException;
 
 public interface Environment {
 
+    String ASSIGN = ":=";
+    String ADD = "+", SUBTRACT = "-", MULTIPLY = "*";
+    String OPEN_BRACKET = "[", CLOSE_BRACKET = "]";
+
     void clear();
     Object process(String line) throws EvaluationException ;
 }
